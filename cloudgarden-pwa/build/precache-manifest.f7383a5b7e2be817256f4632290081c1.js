dfcbec45dc26ea33c394d99528086d0a85314d65
LukeSlev <lslevins@ualberta.ca>
2020-02-19 23:00:07 -0700

    Try adding build to github so pi doesnt need to build

---
+++ cloudgarden-pwa/build/precache-manifest.f7383a5b7e2be817256f4632290081c1.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85a8007f8251a70bb7e1fdc832bafb21",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cbe32222431e8f454fdf",
+    "url": "/static/css/main.851c9585.chunk.css"
+  },
+  {
+    "revision": "cd90f019d322eb05dd50",
+    "url": "/static/js/2.2e2fab99.chunk.js"
+  },
+  {
+    "revision": "79f0ab0d7147b578693d2ce3d40d7684",
+    "url": "/static/js/2.2e2fab99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "837302aac731a1bcff58",
+    "url": "/static/js/3.1fbfdf61.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/static/js/3.1fbfdf61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d126a9bd0f8b24a96c1b",
+    "url": "/static/js/4.ebc8f8a8.chunk.js"
+  },
+  {
+    "revision": "62451c372b0cd00c691e",
+    "url": "/static/js/5.5834b172.chunk.js"
+  },
+  {
+    "revision": "d325ff10bc8b857d95b4",
+    "url": "/static/js/6.b5f2e1ab.chunk.js"
+  },
+  {
+    "revision": "cbe32222431e8f454fdf",
+    "url": "/static/js/main.60ffd11c.chunk.js"
+  },
+  {
+    "revision": "81753e931a7aa37ea7a8",
+    "url": "/static/js/runtime-main.6ecd695b.js"
+  },
+  {
+    "revision": "00429fb9364378d33de629f418f81f2e",
+    "url": "/static/media/aloe_vera.00429fb9.svg"
+  },
+  {
+    "revision": "fa8da0c8c5026590b962bdc6a57273f3",
+    "url": "/static/media/cactus.fa8da0c8.svg"
+  },
+  {
+    "revision": "a8c626e1f7014b54deeb9ef11db301b9",
+    "url": "/static/media/snake_plant.a8c626e1.svg"
+  }
+]);
